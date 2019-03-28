0f1c6160bd79b1114d5ea10b5a8ff5f73b597730
Zhenyu,Liu <liuzhenyulive@live.com>
2019-03-28 23:51:40 +0800

    add- ui

--- EventCloud/4.6.0/angular/src/app/app-routing.module.ts
+++ EventCloud/4.6.0/angular/src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import { AboutComponent } from './about/about.component';
 import { UsersComponent } from './users/users.component';
 import { TenantsComponent } from './tenants/tenants.component';
 import { RolesComponent } from 'app/roles/roles.component';
-import { ChangePasswordComponent } from './users/change-password/change-password.component';
+import { EventsComponent } from 'app/events/events.component';
+import { EventDetailComponent } from 'app/events/event-detail/event-detail.component';
 
 @NgModule({
     imports: [
@@ -16,12 +17,13 @@ import { ChangePasswordComponent } from './users/change-password/change-password
                 path: '',
                 component: AppComponent,
                 children: [
-                    { path: 'home', component: HomeComponent,  canActivate: [AppRouteGuard] },
+                    { path: 'home', component: HomeComponent, canActivate: [AppRouteGuard] },
                     { path: 'users', component: UsersComponent, data: { permission: 'Pages.Users' }, canActivate: [AppRouteGuard] },
                     { path: 'roles', component: RolesComponent, data: { permission: 'Pages.Roles' }, canActivate: [AppRouteGuard] },
                     { path: 'tenants', component: TenantsComponent, data: { permission: 'Pages.Tenants' }, canActivate: [AppRouteGuard] },
-                    { path: 'about', component: AboutComponent },
-                    { path: 'update-password', component: ChangePasswordComponent }
+                    { path: 'events', component: EventsComponent, data: { permission: 'Pages.Events' }, canActivate: [AppRouteGuard] },
+                    { path: 'events/:eventId', component: EventDetailComponent },
+                    { path: 'about', component: AboutComponent }
                 ]
             }
         ])
